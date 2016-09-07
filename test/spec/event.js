ab8fdc8bcfe217f6d66983c798d58fd238f7d771
Saad Shahd <dev.saad.shahd@gmail.com>
2016-09-08 00:16:30 +0200

    Add more specs coverage to event.js

--- test/spec/event.js
+++ test/spec/event.js
@@ -12,12 +12,66 @@ export function test() {
   });
 
   describe('Event on', () => {
-    it('should add listener', () => {
-      window.addEventListener = chai.spy();
+    let spy;
+    let listener;
+    const detail = 'detail';
+
+    beforeEach(() => {
+      spy = chai.spy();
+
+      window.addEventListener = (name, cb) => {
+        listener = cb;
+      };
+    });
+
+    it('should call add addEventListener', () => {
+      window.addEventListener = spy;
 
       Eventer.on('test', () => {});
 
       expect(window.addEventListener).to.have.been.called.with('message');
+    });
+
+    it(`should not invoke the handler if source isn't this window`, () => {
+      Eventer.on('test', spy);
+
+      listener({
+        source: null,
+        data: {
+          type: 'StreamManager:test',
+          detail
+        }
+      });
+
+      expect(spy).to.have.not.been.called();
+    });
+
+    it(`should not invoke the handler wrong event type`, () => {
+      Eventer.on('test1', spy);
+
+      listener({
+        source: null,
+        data: {
+          type: 'StreamManager:test',
+          detail
+        }
+      });
+
+      expect(spy).to.have.not.been.called();
+    });
+
+    it('should call handler with detail', () => {
+      Eventer.on('test', spy);
+
+      listener({
+        source: window,
+        data: {
+          type: 'StreamManager:test',
+          detail
+        }
+      });
+
+      expect(spy).to.have.been.called.with(detail);
     });
   });
 
